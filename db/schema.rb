06133a8f50c5d0ad57c7393f581d427f0b3bee44
Mary Dick <maryclairedick@gmail.com>
2025-06-04 14:26:42 -0500

    new schema for weddings table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_04_191832) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_04_192349) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -23,5 +23,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_04_191832) do
     t.string "password_digest", null: false
     t.string "role", default: "nearlywed", null: false
     t.string "profile_picture_url"
+  end
+
+  create_table "weddings", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.datetime "date"
+    t.string "city"
+    t.string "state"
   end
 end
