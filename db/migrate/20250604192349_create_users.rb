75715d42945153c05398dbdf28cbc401dd2718b7
Mary Dick <maryclairedick@gmail.com>
2025-06-04 14:33:21 -0500

    add foreign key for a wedding to have many users

--- db/migrate/20250604192349_create_users.rb
+++ db/migrate/20250604192349_create_users.rb
@@ -8,6 +8,7 @@ class CreateUsers < ActiveRecord::Migration[8.0]
       t.string :password_digest, null: false
       t.string :role, null: false, default: 'nearlywed'
       t.string :profile_picture_url
+      t.references :wedding, foreign_key: true
     end
   end
 end
