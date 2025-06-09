3dd8ca8d2d3cafacc965f3b6fd7ca18633676ba3
Mary Dick <maryclairedick@gmail.com>
2025-06-09 17:48:16 -0500

    add user auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  resources :users, only: [:create, :show, :index]
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/logged_in', to: 'sessions#is_logged_in?'
 end
