a129b10a0d856d92853d2ea872d6ae62b81d1379
Mary Dick <maryclairedick@gmail.com>
2025-06-11 15:43:19 -0500

    add rspec file for User model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  
+  describe 'validations' do
+    before do
+      @user = User.create(email: "test@example.com", password: "password")
+    end
+
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email).case_insensitive }
+    it { should validate_presence_of(:password) }
+  end
+end
