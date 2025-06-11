6d5c9b6463e079474cb2b9d6683b3f68b9ca81a0
Mary Dick <maryclairedick@gmail.com>
2025-06-11 15:20:12 -0500

    add avatar association to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+    has_one_attached :avatar
     validates :email, presence: true
     validates :email, uniqueness: true
     validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
