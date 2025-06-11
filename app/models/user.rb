61cee21bacf7bf7399c045ae18d7f588e9316f40
Mary Dick <maryclairedick@gmail.com>
2025-06-11 15:42:46 -0500

    use case-insensitive email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
     has_secure_password
     has_one_attached :avatar
     validates :email, presence: true
-    validates :email, uniqueness: true
+    validates :email, uniqueness: { case_sensitive: false }
     validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
