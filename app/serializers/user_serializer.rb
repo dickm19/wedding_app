74ef01f74f5d61792d9bc6b7e3a2479d7f1ab8d4
Mary Dick <maryclairedick@gmail.com>
2025-06-11 15:20:31 -0500

    add avatar_url to the user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,11 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :first_name, :last_name, :role, :profile_picture_url
+  attributes :id, :email, :first_name, :last_name, :role, :avatar_url
+
+  def avatar_url
+    if object.avatar.attached?
+      Rails.application.routes.url_helpers.rails_blob_url(object.avatar)
+    else
+      nil
+    end
+  end
 end
