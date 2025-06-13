d1da82a209a0f4fc6a1c1da07768cc40213dff4c
Mary Dick <maryclairedick@gmail.com>
2025-06-13 16:48:06 -0500

    add spec for users#create action

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -38,4 +38,36 @@ RSpec.describe UsersController, type: :controller do
             expect(response.parsed_body).to include(expected_response)
         end
     end
+
+    describe 'POST #create' do
+        subject(:create) { post :create, params: { user: user_params.merge({ email: "test2@example.com"}) } }
+        before { create }
+
+        it 'returns a success response' do
+            expect(response).to be_successful
+        end
+
+        it 'creates a new user' do
+            expect(User.last.email).to eq("test2@example.com")
+        end
+        
+        context 'when the user has a nearlywed role' do
+            it 'creates a wedding for the user' do
+                expect(User.last.wedding_id).not_to be_nil
+            end
+        end
+
+        context 'when the user has a guest role' do
+            subject(:create_guest) { post :create, params: { user: user_params.merge({ email: "guest@example.com", role: "guest", wedding_id: @user.wedding_id}) } }
+            before { create_guest }
+
+            it 'does not create a wedding for the user' do
+                expect(User.last.wedding).to eq(@user.wedding)
+            end
+
+            it 'adds the user as a guest to the wedding' do
+                expect(@user.wedding.guests).to include(User.last)
+            end
+        end
+    end
 end
