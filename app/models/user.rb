f4f0efb61eaf4a31a7d7453b89a53e1a423434e8
Mary Dick <maryclairedick@gmail.com>
2025-06-13 16:41:45 -0500

    update user associations to allow a wedding to have multiple guests and one user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
-    has_one :wedding_as_newlywed, class_name: "Wedding", foreign_key: "user_id"
+    has_one :wedding_as_nearlywed, class_name: "Wedding", foreign_key: "user_id", dependent: :destroy
     belongs_to :wedding, optional: true
     has_one_attached :avatar
+
     validates :email, presence: true
     validates :email, uniqueness: { case_sensitive: false }
     validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
