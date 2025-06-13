40109a41f868aa0d2a2cf23bed08058b27db9ec0
Mary Dick <maryclairedick@gmail.com>
2025-06-13 13:38:29 -0500

    add different relationships for newlyweds than for guests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
+    has_one :wedding_as_newlywed, class_name: "Wedding", foreign_key: "user_id"
+    belongs_to :wedding, optional: true
     has_one_attached :avatar
     validates :email, presence: true
     validates :email, uniqueness: { case_sensitive: false }
--- app/models/wedding.rb
+++ app/models/wedding.rb
@@ -1,4 +1,6 @@
 class Wedding < ApplicationRecord
-    has_many :users
+    belongs_to :newlywed, -> { where(role: "newlywed") }, class_name: "User", foreign_key: "user_id", optional: true
+    has_many :guests, -> { where(role: "guest") }, class_name: "User"
+
     has_one :registry, dependent: :destroy
 end
