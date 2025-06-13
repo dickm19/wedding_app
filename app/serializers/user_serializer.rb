aa853be33b7145330d0eec6a5894bbd97e7b3aa9
Mary Dick <maryclairedick@gmail.com>
2025-06-13 13:24:13 -0500

    add wedding_id to the user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :first_name, :last_name, :role, :avatar_url
+  attributes :id, :email, :first_name, :last_name, :role, :avatar_url, :wedding_id
 
   def avatar_url
     if object.avatar.attached?
