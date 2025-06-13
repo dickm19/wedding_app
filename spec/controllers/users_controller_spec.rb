8ed16c1fbd7f0e1d95dbd74e735648260234da4b
Mary Dick <maryclairedick@gmail.com>
2025-06-13 16:47:47 -0500

    add spec for users#show action

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-    
+    let(:user_params) do
+        {
+            email: "test@example.com",
+            password: "password",
+            first_name: "John",
+            last_name: "Smith"
+        }
+    end
+
+    let(:expected_response) do
+        {
+            "id" => @user.id,
+            "email" => @user.email,
+            "first_name" => @user.first_name,
+            "last_name" => @user.last_name,
+            "role" => @user.role,
+            "avatar_url" => nil,
+            "wedding_id" => @user.wedding_id
+        }
+    end
+
+    before do
+        @user = User.create(user_params)
+    end
+
+    describe 'GET #show' do
+        subject(:show) { get :show, params: { id: @user.id } }
+
+        before { show }
+        
+        it 'returns a success response' do
+            expect(response).to be_successful
+        end
+        it 'returns the serialized user data' do
+            expect(response.parsed_body).to include(expected_response)
+        end
+    end
 end
