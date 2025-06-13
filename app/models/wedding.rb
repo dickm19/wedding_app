9af7009b928b376d084e9179a9fa3058330bd678
Mary Dick <maryclairedick@gmail.com>
2025-06-13 16:41:59 -0500

    update wedding associations to allow a wedding to have multiple guests and one user

--- app/models/wedding.rb
+++ app/models/wedding.rb
@@ -1,6 +1,7 @@
 class Wedding < ApplicationRecord
-    belongs_to :newlywed, -> { where(role: "newlywed") }, class_name: "User", foreign_key: "user_id", optional: true
-    has_many :guests, -> { where(role: "guest") }, class_name: "User"
+    belongs_to :user, -> { where(role: "nearlyweduser") }
+    has_many :guests, -> { where(role: "guest") }, class_name: "User", foreign_key: "wedding_id"
+    has_many :venues, dependent: :destroy
 
     has_one :registry, dependent: :destroy
 end
