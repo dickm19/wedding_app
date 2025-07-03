bdb736ee1d13f03e6a0c80f139f2ea401ffcfdc1
Mary Dick <maryclairedick@gmail.com>
2025-07-03 15:09:19 -0500

    remove unnecessary formatting

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,8 +2,6 @@ class UserMailer < ApplicationMailer
     def welcome_email
         @user = params[:user]
         @url = "http://localhost:3000"
-        mail(to: @user.email, subject: "Welcome to NearlyWed!") do |format|
-            format.html
-        end
+        mail(to: @user.email, subject: "Welcome to NearlyWed!")
     end
 end
