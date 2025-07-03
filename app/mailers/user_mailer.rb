f7286f5e6861e7caa6b570d3059e0c70ed33c4d2
Mary Dick <maryclairedick@gmail.com>
2025-07-03 15:09:44 -0500

    create guest invite mailer action

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,4 +4,12 @@ class UserMailer < ApplicationMailer
         @url = "http://localhost:3000"
         mail(to: @user.email, subject: "Welcome to NearlyWed!")
     end
+
+    def guest_invite
+        @user = params[:user]
+        @wedding = params[:wedding]
+        @recipient_emails = params[:guest_emails]
+        @url = "http://localhost:3000/guest_signup"
+        mail(to: @recipient_emails, subject: "Save the Date for #{wedding.date}!")
+    end
 end
