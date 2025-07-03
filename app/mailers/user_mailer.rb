bcf33ee039edeea684f27c04f18c584c5744b5dc
Mary Dick <maryclairedick@gmail.com>
2025-07-03 14:58:52 -0500

    add welcome_email action for user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+    def welcome_email
+        @user = params[:user]
+        @url = "http://localhost:3000"
+        mail(to: @user.email, subject: "Welcome to NearlyWed!") do |format|
+            format.html
+        end
+    end
 end
