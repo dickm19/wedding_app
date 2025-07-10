117bdf5b052dfdbacc06ed6504c43733d47e23f7
Mary Dick <maryclairedick@gmail.com>
2025-07-10 16:05:14 -0500

    add email preview for guest_invite email

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -3,4 +3,9 @@ class UserMailerPreview < ActionMailer::Preview
     def welcome_email
         UserMailer.with(user: User.first).welcome_email
     end
+
+    def guest_invite
+        wedding = Wedding.last || Wedding.create(couple_names: "John and Jane", date: "2023-12-31")
+        UserMailer.with(user: User.first, wedding: wedding, guest_emails: ["guest@example.com"]).guest_invite.deliver
+    end
 end
