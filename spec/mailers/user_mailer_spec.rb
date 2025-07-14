b302640b738e0a6f6c6afbd551bad333659ff1cb
Mary Dick <maryclairedick@gmail.com>
2025-07-14 16:19:51 -0500

    add spec for user mailer welcome_email

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,19 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "welcome_email" do
+    let(:user) { User.create(email: "test@example.com") }
+    let(:mail) { UserMailer.with(user: user).welcome_email }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("Welcome to NearlyWed!")
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(["nearlywed1@gmail.com"])
+    end
+
+    it "renders the body" do
+      expect(mail.body.encoded).to match("You have successfully signed up for NearlyWed")
+      expect(mail.body.encoded).to match("Thanks for joining and have a great day!")
+    end
+  end
 end
