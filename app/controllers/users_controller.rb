62651d6ef6dc3fb50012f80c4f13bef01e577545
Mary Dick <maryclairedick@gmail.com>
2025-07-17 15:50:53 -0500

    add full_name to user parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -71,6 +71,6 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:email, :password, :password_confirmation, :avatar, :first_name, :last_name, :rsvp)
+        params.require(:user).permit(:email, :password, :password_confirmation, :avatar, :first_name, :last_name, :rsvp, :full_name)
     end
 end
